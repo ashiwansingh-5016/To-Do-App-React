73e6978fa507f4dd7da63dbd07730cd1e821e360
Ashiwan <ashiwansingh@Ashiwans-MacBook-Pro.local>
2026-04-22 23:06:42 +0530

    Updated Extra Features

--- vite-project/src/feature/body.jsx
+++ vite-project/src/feature/body.jsx
@@ -6,40 +6,48 @@ function Body(){
     const [disc_input , setDisc_input]=useState("") ; 
     const [object , setObject]=useState([]) ;
     const [recent, setRecent] = useState([]);
-    
+    const [Completed, setComplete] = useState([]);
+
+
+
+
+
     const addValue = ()=>{
         setObject([...object , {title:input , Discription : disc_input}])
+        setInput("")
+        setDisc_input("")
+
     };
-
-
     const Delete = (index) => {
         const DeletedItem = object[index]
         setRecent([...recent , DeletedItem])
-    setObject(object.filter((_, i) => i!==index));
+        setObject(object.filter((_, i) => i!==index));
     };
-    const Recent = (index) =>{
-       { recent.map((e , index)=> e[index] )}
+    const Complete = (index)=>{
+        setComplete([...Completed , object[index]])
+        setObject(object.filter((_, i) => i !== index));
+         
     }
+
     return (
         <div>
-            <input onChange={(e)=>setInput(e.target.value)}></input>
-            <input onChange={(e)=>setDisc_input(e.target.value)}></input>
+            <input value={input} onChange={(e)=>setInput(e.target.value)}></input>
+            <input value={input} onChange={(e)=>setDisc_input(e.target.value)}></input>
             <button onClick={addValue}>Add Work</button>
-            <button onClick={(index)=>Recent(index)}> Recently Deleted</button>
             
             
             {object.map((e , index)=>
             <div key={index}>{e.title}-{e.Discription}
             <button onClick={()=>Delete(index)}>Delete</button>
+            <input type="checkbox" onChange={()=>Complete(index)}></input>
             </div>
             )}
-
             <h3>Recently Deleted</h3>
-            {recent.map((e, index) => (
-                <div key={index}>
-                {e.title} - {e.Discription}
-                </div>
-            ))}
+            {recent.map((e, index) => (<div key={index}>{e.title} - {e.Discription}</div>))}
+            <h3>Completed</h3>
+            {Completed.map((e, index) => (<div key={index}>{e.title} - {e.Discription}</div>))}
+            <h3>Working</h3>
+            {object.map((e, index) => (<div key={index}>{e.title} - {e.Discription}</div>))}
         </div>
     )
 }
