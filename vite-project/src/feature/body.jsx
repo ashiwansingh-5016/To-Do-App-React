4d9571949b16ec4b3afcce0cb267ecf0384ab6f1
Ashiwan <ashiwansingh@Ashiwans-MacBook-Pro.local>
2026-04-22 19:14:05 +0530

    Added Recently Deleted Feature

--- vite-project/src/feature/body.jsx
+++ vite-project/src/feature/body.jsx
@@ -13,15 +13,19 @@ function Body(){
 
 
     const Delete = (index) => {
+        const DeletedItem = object[index]
+        setRecent([...recent , DeletedItem])
     setObject(object.filter((_, i) => i!==index));
     };
-
+    const Recent = (index) =>{
+       { recent.map((e , index)=> e[index] )}
+    }
     return (
         <div>
             <input onChange={(e)=>setInput(e.target.value)}></input>
             <input onChange={(e)=>setDisc_input(e.target.value)}></input>
             <button onClick={addValue}>Add Work</button>
-            <button>Recently Deleted</button>
+            <button onClick={(index)=>Recent(index)}> Recently Deleted</button>
             
             
             {object.map((e , index)=>
@@ -29,6 +33,13 @@ function Body(){
             <button onClick={()=>Delete(index)}>Delete</button>
             </div>
             )}
+
+            <h3>Recently Deleted</h3>
+            {recent.map((e, index) => (
+                <div key={index}>
+                {e.title} - {e.Discription}
+                </div>
+            ))}
         </div>
     )
 }
